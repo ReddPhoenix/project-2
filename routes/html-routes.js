201b767714f47288371c1caf2424e228208d1bf2
Trever O <toveson9522@gmail.com>
2020-10-28 18:38:26 -0600

    fixed routes so they are now working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,23 @@
 // Dependencies
-var path = require(path);
+var path = require('path');
 
 // Routes
 module.exports = function (app) {
     // customers route
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/customer.html'))
+    app.get('/customers', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/customer.html'));
     });
     // dashboard route
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/dashboard.html'))
+    app.get('/dashboard', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/dashboard.html'));
     });
     // inventory route
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/inventory.html'))
+    app.get('/inventory', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/inventory.html'));
     });
     // login route
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/login.html'))
+        res.sendFile(path.join(__dirname, '../public/login.html'));
     });
 
 };
