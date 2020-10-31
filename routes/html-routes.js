57273ee51065276ab2be25a6b6059d5d4d64e740
Trever O <toveson9522@gmail.com>
2020-10-31 12:19:27 -0600

    added passport configuration

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,26 +1,28 @@
 // Dependencies
 var path = require('path');
 
+var isAuthenticated = require('../config/middleware/isAuthenticated');
+
 // Routes
 module.exports = function (app) {
     // customers route
-    app.get('/customers', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/customer.html'));
+    app.get('/customers', isAuthenticated, function (req, res) {
+        res.render(path.join(__dirname, isAuthenticated, '../public/customer.html'));
     });
     // dashboard route // main.handlebars route
     app.get('/main', function (req, res) {
-        res.sendFile(path.join(__dirname, '../views/layouts/main.handlebars'));
+        res.render(path.join(__dirname, '../views/layouts/main.handlebars'));
     });
     // inventory route
-    app.get('/inventory', (req, res) => {
-        res.render('inventory', { title: 'Inventory' });
+    app.get('/inventory', isAuthenticated, (req, res) => {
+        res.render('inventory', isAuthenticated, { title: 'Inventory' });
     });
     // login route
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/login.html'));
     });
     // orders route
-    app.get('/orders', function (req, res) {
+    app.get('/orders', isAuthenticated, function (req, res) {
         res.sendFile(path.join(__dirname, '../public/orders.html'));
     });
 };
